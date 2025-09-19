c3ba6753bd9c4c1e55a68e39d9cdde58ca01cc23
Hellorld! <jeremy.thornton@nhs.net>
2025-09-19 10:19:25 +0100

    Update edlin_types.h

--- src/EDLIN/edlin_types.h
+++ src/EDLIN/edlin_types.h
@@ -9,10 +9,19 @@ typedef uint16_t edlin_size_t;
 typedef char edlin_line_t[EDLIN_LINE_SIZE];
 
 typedef struct {
+    const char *filename;
+    edlin_size_t capacity;
+    bool help;
+    bool verbose;
+} edlin_config_t;
+
+typedef struct {
     edlin_line_t path;
     edlin_line_t* lines;
     edlin_size_t size;
     edlin_size_t capacity;
 } edlin_file_t;
+
+
 
 #endif
