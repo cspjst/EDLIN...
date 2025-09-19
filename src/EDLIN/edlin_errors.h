9bfaf3b14fbbb0dd1dcef10d235aebb16c71dc48
Hellorld! <jeremy.thornton@nhs.net>
2025-09-19 14:52:03 +0100

    Update edlin_errors.h

--- src/EDLIN/edlin_errors.h
+++ src/EDLIN/edlin_errors.h
@@ -3,12 +3,11 @@
 
 typedef enum {
     EDLIN_SUCCESS = 0,
-
     EDLIN_ERR_MISSING_ARG,
     EDLIN_ERR_MULTIPLE_FILENAMES,
     EDLIN_ERR_UKNOWN_OPT
 } edlin_err_t;
 
-void* edlin_panic(edlin_err_t e); 
+void* edlin_panic(edlin_err_t e, const char* context); 
 
 #endif
