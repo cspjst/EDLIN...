8d14a7cf4147bfcaf2ef58a1973911b27039c2ae
Hellorld! <jeremy.thornton@nhs.net>
2025-09-19 14:52:41 +0100

    Create edlin_errors.c

---
+++ src/EDLIN/edlin_errors.c
@@ -0,0 +1,12 @@
+#include "edlin_errors.h" 
+
+const char* const EDLIN_ERROR_MESSAGES[] = {
+    "Success!",
+    "Missing argument!",
+    "Multiple filenames!",
+    "Unknown option!"
+}
+
+void* edlin_panic(edlin_err_t e, const char* context) {
+  printf(edlin_err_t
+}
