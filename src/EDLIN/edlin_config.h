0e035bb2661549d900575e1d562bb6018cbdf51e
Hellorld! <jeremy.thornton@nhs.net>
2025-09-19 13:37:51 +0100

    Update and rename edlin_config_parse.h to edlin_config.h

---
+++ src/EDLIN/edlin_config.h
@@ -0,0 +1,8 @@
+#ifndef EDLIN_PARSE_H
+#define EDLIN_PARSE_H
+
+#include "edlin_types.h"
+
+bool edlin_config((int argc, char* argv[], edlin_config_t* config);
+
+#endif
--- src/EDLIN/edlin_config_parse.h
+++
@@ -1,8 +0,0 @@
-#ifndef EDLIN_CONFIG_PARSE_H
-#define EDLIN_CONFIG_PARSE_H
-
-#include "edlin_types.h"
-
-bool edlin_config_parse((int argc, char* argv[], edlin_config_t* config);
-
-#endif
