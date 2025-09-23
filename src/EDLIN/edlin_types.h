4d6f973bd465a79da4a2e146a2f50134e409b35f
Hellorld! <jeremy.thornton@nhs.net>
2025-09-23 16:42:46 +0100

    Update edlin_types.h

--- src/EDLIN/edlin_types.h
+++ src/EDLIN/edlin_types.h
@@ -9,4 +9,19 @@ typedef uint16_t edlin_size_t;
 
 typedef char edlin_line_t[EDLIN_LINE_SIZE];
 
+typedef struct {
+    edlin_line_t path;
+    edlin_line_t** lines;   // array of pointers to line buffers
+    edlin_size_t pos;       // insert position line number
+    edlin_size_t size;
+    edlin_size_t capacity;
+} edlin_file_t;
+
+typedef struct {
+    edlin_line_t line;
+    edlin_token_t op;
+    edlin_size_t argc;
+    char* argv[EDLIN_ARGC_MAX];
+} edlin_cmd_t;
+
 #endif
