6d258bd458c27a5722684f85aff6da46e715a5e6
Hellorld! <jeremy.thornton@nhs.net>
2025-09-23 16:44:39 +0100

    Update edlin_types.h

--- src/EDLIN/edlin_types.h
+++ src/EDLIN/edlin_types.h
@@ -7,6 +7,27 @@
 
 typedef uint16_t edlin_size_t;
 
+typedef enum {
+    TOK_EDIT = 0,   // line# Edit a single line
+    TOK_HELP,       // ? Show help
+    TOK_END,        // E End (save file)
+    TOK_QUIT,       // Q Quit (throw away changes)
+    TOK_APPEND,     // [#lines]A Append a line below the mark
+    TOK_COPY,       // [range],toline[,times]C	Copy lines
+    TOK_DELETE,     // [range]D Delete lines
+    TOK_INSERT,     // [line]I Insert new lines before the mark
+    TOK_LIST,       // [range]L	List the file (mark at top unless set elsewhere)
+    TOK_MOVE,       // [range],tolineM Move lines
+    TOK_PAGE,       // [range]P	Page (same as List, but mark end of list)
+    TOK_WRITE,      // [#lines]W Write the file to disk
+    TOK_REPLACE,    // [range][?]Rold,new Replace text
+    TOK_SEARCH,     // [range][?]S[string] Search for text
+    TOK_TRANSFER,   // [toline]Tfilepath Transfer (insert the contents of a new file at the mark)
+    TOK_ERROR,      // recognised command but syntax error
+    TOK_EMPTY,      // user hit return on whitespace line
+    TOK_UNKNOWN
+} edlin_token_t;
+
 typedef char edlin_line_t[EDLIN_LINE_SIZE];
 
 typedef struct {
