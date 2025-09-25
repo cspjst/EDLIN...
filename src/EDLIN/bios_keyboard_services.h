38854c5ee965e9fdffc7e1188b592b0a4e28fb02
Hellorld! <jeremy.thornton@nhs.net>
2025-09-25 11:29:04 +0100

    Update bios_keyboard_services.h

--- src/EDLIN/bios_keyboard_services.h
+++ src/EDLIN/bios_keyboard_services.h
@@ -1,22 +1,70 @@
 /**
+* @url https://www.stanislavs.org/helppc/int_16.html
+*
+*   INT 16,0   Wait for keystroke and read
+* 	INT 16,1   Get keystroke status
+* 	INT 16,2   Get shift status
+* 	INT 16,3   Set keyboard typematic rate (AT+)
+* 	INT 16,4   Keyboard click adjustment (AT+)
+* 	INT 16,5   Keyboard buffer write  (AT,PS/2 enhanced keyboards)
+* 	INT 16,10  Wait for keystroke and read  (AT,PS/2 enhanced keyboards)
+* 	INT 16,11  Get keystroke status  (AT,PS/2 enhanced keyboards)
+* 	INT 16,12  Get shift status  (AT,PS/2 enhanced keyboards)
+*   
 * INT 16h Detailed Function List
-* Function	                            (AH)	    Device	    Inputs	                      Return
-* Read key press	                        0h	      Keyboard	  None	                        AH[1] = Scan code of the key pressed down	AL = ASCII character of the button pressed
-* Get the State of the keyboard buffer	  1h	      Keyboard	  None	                        AH = Scan code, ZF = 0 if key pressed down	AL = ASCII character, AX = 0 if no Scan codes
-* Get the State of the keyboard	          2h	      Keyboard	  None	                        AH = BIOS Keyboard flags
-* Establish repetition factor	            3h	      Keyboard	  AL = Mode                     None 
-*                                                               BH = Repeat delay (if AL = 5) 
-*                                                               BL = Typematic rate	
-* Keyboard Click adjustment	              4h	      Keyboard	  AL = 0 (off) or 1 (on)	      None
-* Simulate a keystroke	                  5h	      Keyboard	  CH = Scan code
-*                                                               CL = ASCII Character	        AL = 0 If successful, 1 otherwise
-* Get the ID of the keyboard	            Ah	      Keyboard	  None	                        AL = Keyboard Id
+* Function	                            (AH)	    Device	    Inputs	                        System   Return
+* Read key press	                      0h	      Keyboard	  None	                        PC/XT    AH[1] = Scan code of the key pressed down	AL = ASCII character of the button pressed
+* Get the State of the keyboard buffer	  1h	      Keyboard	  None	                        PC/XT    AH = Scan code, ZF = 0 if key pressed down	AL = ASCII character, AX = 0 if no Scan codes
+* Get the State of the keyboard	          2h	      Keyboard	  None	                        PC/XT    AH = BIOS Keyboard flags
+* Establish repetition factor	          3h	      Keyboard	  AL = Mode                     AT+      None 
+*                                                                 BH = Repeat delay (if AL = 5) 
+*                                                                 BL = Typematic rate	
+* Keyboard Click adjustment	              4h	      Keyboard	  AL = 0 (off) or 1 (on)	    AT+      None
+* Simulate a keystroke	                  5h	      Keyboard	  CH = Scan code                AT,PS/2  AL = 0 If successful, 1 otherwise
+*                                                                 CL = ASCII Character	        
 */
 #ifndef BIOS_KEYBOARD_SERVICES_H
 #define BIOS_KEYBOARD_SERVICES_H
 
+#include <stdint.h>
+
+typedef union {
+    uint16_t key;
+    typedef struct {  
+        uint8_t ascii;
+        uint8_t scan;
+    } parts;
+} bios_key_t
+
+typedef uint8_t bios_keybd_info_t;
+
 #define BIOS_KEYBOARD_SERVICES    16h
 
+#define BIOS_WAIT_FOR_KEYSTROKE_AND_READ      0
+#define BIOS_GET_KEYSTROKE_STATUS             1 
+#define BIOS_GET_SHIFT_STATUS                 2
+#define BIOS_SET_KEYBOARD_TYPEMATIC           3
+#define BIOS_KEYBOARD_CLICK_ADJUSTMENT        4
+#define BIOS_KEYBOARD_BUFFER_WRITE            5
 
+// #define BIOS_WAIT_FOR_KEYSTROKE_AND_READ      10
+// #define BIOS_GET_KEYSTROKE_STATUS             11
+// #define BIOS_GET_SHIFT_STATUS                 12
+
+bios_key_t wait_for_keystroke_and_read();
+
+bios_key_t get_keystroke_status();
+
+bios_keybd_info_t get_shift_status();
+
+void set_keyboard_typematic(uint8_t mode, uint8_t delay, uint8_t rate);
+
+void keyboard_click_adjustment(bool state);
+
+bool keyboard_buffer_write(bios_key_t key);
+
+// wait_for_keystroke_and_read
+// get_keystroke_status
+// get_shift_status
 
 #endif
