a9644b4060723c827ae6a570e08d337c7aa4b22a
Hellorld! <jeremy.thornton@nhs.net>
2025-09-26 12:03:45 +0100

    Update bios_keyboard_services.h

--- src/EDLIN/bios_keyboard_services.h
+++ src/EDLIN/bios_keyboard_services.h
@@ -14,43 +14,8 @@
 #ifndef BIOS_KEYBOARD_SERVICES_H
 #define BIOS_KEYBOARD_SERVICES_H
 
-#include <stdint.h>
-
-#define BIOS_KEY_RIGHT_SHIFT    1
-#define BIOS_KEY_LEFT_SHIFT     2
-#define BIOS_KEY_CTRL           4
-#define BIOS_KEY_ALT            8
-#define BIOS_SCROLL_LOCK        16
-#define BIOS_NUM_LOCK           32
-#define BIOS_CAPS_LOCK          64
-#define BIOS_KEY_INSERT         128
-
-typedef uint8_t bios_keybd_info_t;
-
-#pragma pack(1)
-typedef struct {
-    uint8_t ascii;
-    uint8_t scan;
-} bios_key_parts_t;
-#pragma pack()
-
-typedef union {
-    uint16_t combined;
-    bios_key_parts_t parts;
-} bios_key_t;
-
-#pragma pack(1)
-typedef struct {
-    bios_key_t key;
-    uint8_t is_pressed;
-} bios_key_event_t;
-#pragma pack()
-
-#define BIOS_KEYBOARD_SERVICES    16h
-
-#define BIOS_WAIT_FOR_KEYSTROKE_AND_READ      0
-#define BIOS_GET_KEYSTROKE_STATUS             1
-#define BIOS_GET_SHIFT_STATUS                 2
+#include "bios_keyboard_constants.h
+#include "bios_keybaord_types.h"
 
 void bios_wait_for_keystroke_and_read(bios_key_t* key);
 
