3dd9a0ea4ba8b841c353f13ecfd66ae47dea284b
Hellorld! <jeremy.thornton@nhs.net>
2025-09-26 12:03:30 +0100

    Create bios_keyboard_types.h

---
+++ src/EDLIN/bios_keyboard_types.h
@@ -0,0 +1,27 @@
+#ifndef BIOS_KEYBOARD_TYPES_H
+#define BIOS_KEYBOARD_TYPES_H
+
+#include <stdint.h>
+
+typedef uint8_t bios_keybd_info_t;
+
+#pragma pack(1)
+typedef struct {
+    uint8_t ascii;
+    uint8_t scan;
+} bios_key_parts_t;
+#pragma pack()
+
+typedef union {
+    uint16_t combined;
+    bios_key_parts_t parts;
+} bios_key_t;
+
+#pragma pack(1)
+typedef struct {
+    bios_key_t key;
+    uint8_t is_pressed;
+} bios_key_event_t;
+#pragma pack()
+
+#endif
