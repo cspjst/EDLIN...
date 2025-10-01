87ff3e7a2b954d34fab2c0a9dbd130829a818a0b
Hellorld! <jeremy.thornton@nhs.net>
2025-10-01 12:04:52 +0100

    Update edlin_constants.h

--- src/EDLIN/edlin_constants.h
+++ src/EDLIN/edlin_constants.h
@@ -2,8 +2,9 @@
 #define EDLIN_CONSTANTS_H
 
 // ASCII control codes
-#define CTRL_C  3       // ETX  end of text
-#define CTRL_Z  26      // SUB  substitute
+//#define CTRL_C        // ETX  end of text
+//#define CTRL_Z        // edlin uses to encode control chars
+#define CTRL_Z  '\x1A'      // SUB  substitute
 
 #define EDLIN_LINE_SIZE  256
 #define EDLIN_PAGE_SIZE 23
