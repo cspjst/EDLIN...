f35e8d233df784c3732f063624e9c0ea6ceebd2e
Hellorld! <jeremy.thornton@nhs.net>
2025-10-01 08:45:55 +0100

    Update edlin_types.h

--- src/EDLIN/edlin_types.h
+++ src/EDLIN/edlin_types.h
@@ -10,32 +10,33 @@ typedef uint16_t edlin_size_t;
 // use sequential char values to in-place tokenize and make separating out command types easier using > and <
 typedef enum {
     TOK_ERROR = 0,  // error
-
-    TOK_EDIT = 'A', // A    line# Edit a single line
+    // edit command
+    TOK_EDIT,       // line# Edit a single line
     // no arg commands
-    TOK_HELP,       // B    ? Show help
-    TOK_END,        // C    E End (save file)
-    TOK_QUIT,       // D    Q Quit (throw away changes)
+    TOK_HELP,       // ? Show help
+    TOK_END,        // E End (save file)
+    TOK_QUIT,       // Q Quit (throw away changes)
     // pre command arg(s)
-    TOK_APPEND,     // E    [#lines]A Append a line below the mark
-    TOK_COPY,       // F    [range],toline[,times]C	Copy lines
-    TOK_DELETE,     // G    [range]D Delete lines
-    TOK_INSERT,     // H    [line]I Insert new lines before the mark
-    TOK_LIST,       // I    [range]L	List the file (mark at top unless set elsewhere)
-    TOK_MOVE,       // J    [range],tolineM Move lines
-    TOK_PAGE,       // K    [range]P	Page (same as List, but mark end of list)
-    TOK_WRITE,      // L    [#lines]W Write the file to disk
+    TOK_APPEND,     // [#lines]A Append a line below the mark
+    TOK_COPY,       // [range],toline[,times]C	Copy lines
+    TOK_DELETE,     // [range]D Delete lines
+    TOK_INSERT,     // [line]I Insert new lines before the mark
+    TOK_LIST,       // [range]L	List the file (mark at top unless set elsewhere)
+    TOK_MOVE,       // [range],tolineM Move lines
+    TOK_PAGE,       // [range]P	Page (same as List, but mark end of list)
+    TOK_WRITE,      // [#lines]W Write the file to disk
     // pre & post query commnnd arg(s)
-    TOK_REPLACE,    // M    [range][?]Rold,new Replace text
-    TOK_SEARCH,     // N    [range][?]S[string] Search for text
+    TOK_REPLACE,    // [range]Rold,new Replace text
+    TOK_SEARCH,     // [range]S[string] Search for text
+    TOK_QREPLACE,   // [range]?Rold,new Replace text
+    TOK_QSEARCH,    // [range]?S[string] Search for text
     // pre & post commnnd args
-    TOK_TRANSFER,   // O    [toline]Tfilepath Transfer (insert the contents of a new file at the mark)
+    TOK_TRANSFER,   // [toline]Tfilepath Transfer (insert the contents of a new file at the mark)
     // special cases
-    TOK_EMPTY,      // P    user hit return on whitespace line
-    TOK_QUERY,      // Q    ? is interactive
+    TOK_EMPTY,      // user hit return on whitespace line
     // error states
-    TOK_SYNTAX,     // R    command ok but args wrong
-    TOK_UNKNOWN     // S
+    TOK_SYNTAX,     // command ok but args wrong
+    TOK_UNKNOWN     // 
 } enum_token_t;
 
 typedef char edlin_line_t[EDLIN_LINE_SIZE];
